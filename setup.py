962c583ebe362b0019666287f549abf7484b4304
Nikita <iamlilacpenguin@gmail.com>
2018-02-09 19:48:36 +0300

    added mzxml files support

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setup(name='maxquant',
           'console_scripts': [
               'maxquant-batch = maxquant.cli.maxquant_batch:main',
               'maxquant-mqpar = maxquant.cli.maxquant_mqpar:main',
+              'maxquant-mzxml = maxquant.cli.mzxml:main',
           ]
       }, install_requires=['ujson', 'simple-drmaa-scheduler>=0.0.6']
       )
