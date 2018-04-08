f67ef6e88083ac9296673c5d47772e250158f8f3
Nikita Prianichnikov <lpenguin@yandex.ru>
2018-04-08 16:29:28 +0300

    removed preprocessing commands

--- setup.py
+++ setup.py
@@ -4,13 +4,11 @@ setup(name='maxquant',
       packages=['maxquant',
                 'maxquant.cli',
                 ],
-      version='0.1.1',
+      version='0.1.0',
       entry_points={
           'console_scripts': [
               'maxquant-batch = maxquant.cli.maxquant_batch:main',
               'maxquant-mqpar = maxquant.cli.maxquant_mqpar:main',
-              # 'maxquant-mzxml = maxquant.cli.mzxml:main',
-              # 'maxquant-mzml = maxquant.cli.mzml:main',
           ]
       }, install_requires=['ujson', 'simple-drmaa-scheduler>=0.0.6']
       )
